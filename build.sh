a7243b177d5a9c927dd4b4277d97bc238e4f5a0b
thorek1 <thorek1@users.noreply.github.com>
2020-07-12 14:07:32 +0200

    rewrote build script

--- build.sh
+++ build.sh
@@ -15,22 +15,22 @@ echo 'Exporting repo keys...'
 export APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=DontWarn
 apt-key exportall > apt-trusted-keys
 
-echo 'Building cuda-jetpack:4.4-base container...'
-sudo DOCKER_BUILDKIT=1 docker build \
-    -t cuda-jetpack:4.4-base \
-    -f Dockerfile.base .
+#echo 'Building cuda-jetpack:4.4-base container...'
+#sudo DOCKER_BUILDKIT=1 docker build \
+#    -t cuda-jetpack:4.4-base \
+#    -f Dockerfile.base .
 
-echo 'Building cuda-jetpack:4.4-runtime container...'
-sudo DOCKER_BUILDKIT=1 docker build \
-    -t cuda-jetpack:4.4-runtime \
-    --build-arg BASE_IMAGE=cuda-jetpack:4.4-base \
-    -f Dockerfile.runtime .
+#echo 'Building cuda-jetpack:4.4-runtime container...'
+#sudo DOCKER_BUILDKIT=1 docker build \
+#    -t cuda-jetpack:4.4-runtime \
+#    --build-arg BASE_IMAGE=cuda-jetpack:4.4-base \
+#    -f Dockerfile.runtime .
 
-echo 'Building cuda-jetpack:4.4-devel container...'
-sudo DOCKER_BUILDKIT=1 docker build \
-    -t cuda-jetpack:4.4-devel \
-    --build-arg BASE_IMAGE=cuda-jetpack:4.4-runtime \
-    -f Dockerfile.devel .
+#echo 'Building cuda-jetpack:4.4-devel container...'
+#sudo DOCKER_BUILDKIT=1 docker build \
+#    -t cuda-jetpack:4.4-devel \
+#    --build-arg BASE_IMAGE=cuda-jetpack:4.4-runtime \
+#    -f Dockerfile.devel .
 
 echo 'Building cuda-jetpack:4.4-julia_coder container...'
 sudo DOCKER_BUILDKIT=1 docker build \
@@ -38,4 +38,4 @@ sudo DOCKER_BUILDKIT=1 docker build \
     --build-arg BASE_IMAGE=cuda-jetpack:4.4-devel \
     -f Dockerfile.julia_coder .
 
-sudo docker tag cuda-jetpack:4.4-runtime cuda-jetpack:latest
+#sudo docker tag cuda-jetpack:4.4-runtime cuda-jetpack:latest
