fc48c9b29a98e06c5286d1ba9bbf1fb2f1eb83cb
thorek1 <thorek1@users.noreply.github.com>
2020-07-15 09:26:21 +0200

    qz cpp

---
+++ real_qz_calc.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+//#include <string>
+#include <Eigen/Eigenvalues>
+//#include <Eigen/Dense>
+
+using namespace Eigen;
+using namespace std;
+//using Eigen::MatrixXf;
+//using Eigen::RealQZ;
+//using std::cout;
+int main(){
+MatrixXf A = MatrixXf::Random(4,4);
+MatrixXf B = MatrixXf::Random(4,4);
+RealQZ<MatrixXf> qz(A,B); // preallocate space for 4x4 matrices
+//qz.compute(A,B);  // A = Q S Z,  B = Q T Z
+
+// print original matrices and result of decomposition
+cout << "A:\n" << A << "\n" << "B:\n" << B << "\n";
+cout << "S:\n" << qz.matrixS() << "\n" << "T:\n" << qz.matrixT() << "\n";
+cout << "Q:\n" << qz.matrixQ() << "\n" << "Z:\n" << qz.matrixZ() << "\n";
+
+// verify precision
+cout << "\nErrors:"
+  << "\n|A-QSZ|: " << (A-qz.matrixQ()*qz.matrixS()*qz.matrixZ()).norm()
+  << ", |B-QTZ|: " << (B-qz.matrixQ()*qz.matrixT()*qz.matrixZ()).norm()
+  << "\n|QQ* - I|: " << (qz.matrixQ()*qz.matrixQ().adjoint() - MatrixXf::Identity(4,4)).norm()
+  << ", |ZZ* - I|: " << (qz.matrixZ()*qz.matrixZ().adjoint() - MatrixXf::Identity(4,4)).norm()
+  << "\n";
+return 0;
+}
